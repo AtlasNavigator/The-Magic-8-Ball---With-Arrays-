5d370242e5aa1f9b1affcbb011509fe1f0c438ff
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-02-01 21:44:45 +0000

    Working random options!

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const readLine = require('readline-sync'); //readLine-sync
+
+let possibilities = ["As I see it, yes", "It is certain", "It is decidedly so", "Most likely", "Outlook good", "Signs point to yes", "Without a doubt", "Yes", "Yes - definitely", "You may rely on it", "Reply hazy, try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]; // List of possible outputs
+
+// The random generator choosing items from the array above
+let randomChoice = possibilities[Math.floor(Math.random(19)*possibilities.length)];
+
+console.log(randomChoice); //Log the output to the console
